245163a8fba67f74d9b6d3ae8926739d37cee7eb
laizhili <969414747@qq.com>
2025-03-25 21:07:08 +0800

    provider利用高性能的NIO框架Vert.x来作为RPC框架的web服务器

--- provider/src/main/java/com/scut/provider/EasyProvider.java
+++ provider/src/main/java/com/scut/provider/EasyProvider.java
@@ -1,12 +1,17 @@
 package com.scut.provider;
 
 
+import com.scut.server.HttpServer;
+import com.scut.server.VertxHttpServer;
+
 /**
  * 简易服务提供者示例
  */
 public class EasyProvider {
 
     public static void main(String[] args) {
-        // 提供服务
+        // 启动 web 服务
+        HttpServer server = new VertxHttpServer();
+        server.doStart(8080);
     }
 }
