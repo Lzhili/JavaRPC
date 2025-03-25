56daa77a8d0e16ba31d49e607f81a8e284b43ace
laizhili <969414747@qq.com>
2025-03-25 21:18:31 +0800

    添加本地服务注册

--- provider/src/main/java/com/scut/provider/EasyProvider.java
+++ provider/src/main/java/com/scut/provider/EasyProvider.java
@@ -1,6 +1,8 @@
 package com.scut.provider;
 
 
+import com.scut.common.service.UserService;
+import com.scut.registry.LocalRegistry;
 import com.scut.server.HttpServer;
 import com.scut.server.VertxHttpServer;
 
@@ -10,6 +12,9 @@ import com.scut.server.VertxHttpServer;
 public class EasyProvider {
 
     public static void main(String[] args) {
+        //本地注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
         // 启动 web 服务
         HttpServer server = new VertxHttpServer();
         server.doStart(8080);
