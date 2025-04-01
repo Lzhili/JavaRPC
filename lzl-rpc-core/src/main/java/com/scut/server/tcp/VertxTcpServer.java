3aab09a1e28190c8dfc47f886d7a89b9838742a0
laizhili <969414747@qq.com>
2025-04-01 16:33:59 +0800

    成功测试利用Vert.x内置的RecordParser来解决半包和粘包问题

--- lzl-rpc-core/src/main/java/com/scut/server/tcp/VertxTcpServer.java
+++ lzl-rpc-core/src/main/java/com/scut/server/tcp/VertxTcpServer.java
@@ -1,17 +1,13 @@
 package com.scut.server.tcp;
 
 import com.scut.server.HttpServer;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.parsetools.RecordParser;
 
 public class VertxTcpServer implements HttpServer {
-
-//    private byte[] handleRequest(byte[] requestData) {
-//        // 在这里编写处理请求的逻辑，根据 requestData 构造响应数据并返回
-//        // 这里只是一个示例，实际逻辑需要根据具体的业务需求来实现
-//        return "Hello, client!".getBytes();
-//    }
 
     @Override
     public void doStart(int port) {
@@ -22,18 +18,29 @@ public class VertxTcpServer implements HttpServer {
         NetServer server = vertx.createNetServer();
 
         // 处理请求
-        server.connectHandler(new TcpServerHandler());
-//        server.connectHandler(socket -> {
-//            // 处理连接
-//            socket.handler(buffer -> {
-//                // 处理接收到的字节数组
-//                byte[] requestData = buffer.getBytes();
-//                // 在这里进行自定义的字节数组处理逻辑，比如解析请求、调用服务、构造响应等
-//                byte[] responseData = handleRequest(requestData);
-//                // 向连接到服务器的客户端发送响应数据。
-//                socket.write(Buffer.buffer(responseData));
-//            });
-//        });
+//        server.connectHandler(new TcpServerHandler());
+
+        //测试粘包和半包
+        server.connectHandler(socket -> {
+            String testMessage = "Hello, server!Hello, server!Hello, server!Hello, server!";
+            int messageLength = testMessage.getBytes().length;
+
+            // 构造parser
+            RecordParser parser = RecordParser.newFixed(messageLength);
+            parser.setOutput(new Handler<Buffer>() {
+
+                @Override
+                public void handle(Buffer buffer) {
+                    String str = new String(buffer.getBytes());
+                    System.out.println(str);
+                    if (testMessage.equals(str)) {
+                        System.out.println("good");
+                    }
+                }
+            });
+
+            socket.handler(parser);
+        });
 
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
