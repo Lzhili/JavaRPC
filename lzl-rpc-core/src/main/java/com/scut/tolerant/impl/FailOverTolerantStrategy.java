2d2a01994d5a457eaa53ee43b92a4e37048d866e
laizhili <969414747@qq.com>
2025-04-04 20:38:50 +0800

    补充实现fail-over故障转移的容错机制

--- lzl-rpc-core/src/main/java/com/scut/tolerant/impl/FailOverTolerantStrategy.java
+++ lzl-rpc-core/src/main/java/com/scut/tolerant/impl/FailOverTolerantStrategy.java
@@ -1,8 +1,13 @@
 package com.scut.tolerant.impl;
+import com.scut.model.RpcRequest;
 import com.scut.model.RpcResponse;
+import com.scut.model.ServiceMetaInfo;
+import com.scut.server.tcp.VertxTcpClient;
 import com.scut.tolerant.TolerantStrategy;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,8 +18,23 @@ public class FailOverTolerantStrategy implements TolerantStrategy {
 
     @Override
     public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
-        // todo 可自行扩展，获取其他服务节点并调用
-        return null;
+        log.info("故障转移异常", e);
+
+        // context里存储request,selectServiceMetaInfoList
+        List<ServiceMetaInfo> serviceMetaInfoList =
+                (List<ServiceMetaInfo>) context.getOrDefault("serviceMetaInfoList", new ArrayList<>());
+        if (serviceMetaInfoList.isEmpty()) {
+            throw new RuntimeException("暂无可用服务", e);
+        }
+        RpcRequest rpcRequest = (RpcRequest) context.getOrDefault("rpcRequest", null);
+        try {
+            // 每个服务都试一次
+            return VertxTcpClient.doRequest(rpcRequest, serviceMetaInfoList.get(0));
+        } catch (Exception err) {
+            serviceMetaInfoList.remove(0);
+            context.put("serviceMetaInfoList", serviceMetaInfoList);
+            return doTolerant(context, e);
+        }
     }
 }
 
